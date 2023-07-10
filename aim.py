654d3632a3014a67c99eec43151c84a0bc55bc30
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2023-07-10 19:34:43 +0800

    。

--- aim.py
+++ aim.py
@@ -5,9 +5,9 @@ def aim(bucket_x, bucket_y, ix, iy, last_error_x, last_error_y, vehicle):
     flag = 0#flag aimed
     # PID
     dt = 0.1
-    kp = 0.002  # 比例参数
-    ki = 0.001  # 积分参数
-    kd = 0.005  # 微分参数
+    kp = 0.0002  # 比例参数
+    ki = 0.0001  # 积分参数
+    kd = 0.0005  # 微分参数
     
     self_x = 320
     self_y = 240
